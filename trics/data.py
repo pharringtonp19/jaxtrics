b7adf590c1666e13dfd2d1458995d0fbedb41f40
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-03-17 11:14:38 -0400

    Update data.py

--- trics/data.py
+++ trics/data.py
@@ -1,8 +1,12 @@
 
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 from jaxlib.xla_extension import ArrayImpl
 
 class Data(NamedTuple):
     X: ArrayImpl
+    Z: Optional[ArrayImpl] = None
     D: ArrayImpl
     Y: ArrayImpl
+
+    def __new__(cls, X, D, Y, Z=None):
+        return super().__new__(cls, X, Z, D, Y)

aa09290f1d47448038b0e6122a22a5a5bbe8a676
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-21 15:59:16 -0400

    Update ocr.py

--- trics/nlp/ocr.py
+++ trics/nlp/ocr.py
@@ -3,6 +3,10 @@ from typing import List
 from pdf2image import convert_from_path
 from PIL import Image
 import pytesseract
+from PyPDF2 import PdfReader, PdfWriter
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+
 
 def get_text(doc: str, first_page: int = 1, last_page: int = 2) -> str:
     """
@@ -32,4 +36,53 @@ def cleanup_temp_file(temp_filename):
     """Remove the temporary file if it exists."""
     if os.path.exists(temp_filename):
         os.remove(temp_filename)
+
+def extract_pages(source_file, num_pages):
+    """Extracts the first 'num_pages' from the source PDF file and saves it to a temporary file."""
+    reader = PdfReader(source_file)
+    pdf_writer = PdfWriter()
+
+    for page in range(min(num_pages, len(reader.pages))):
+        pdf_writer.add_page(reader.pages[page])
+
+    with open('temp.pdf', 'wb') as temp_pdf:
+        pdf_writer.write(temp_pdf)
+
+def read_pdf_with_azure(temp_filename, computervision_client):
+    """Uses Azure Computer Vision to read text from a PDF stored in 'temp_filename'."""
+    try:
+        with open(temp_filename, "rb") as pdf:
+            read_response = computervision_client.read_in_stream(pdf, raw=True)
+        operation_location_remote = read_response.headers["Operation-Location"]
+        operation_id = operation_location_remote.split("/")[-1]
+
+        while True:
+            read_result = computervision_client.get_read_result(operation_id)
+            if read_result.status not in ['notStarted', 'running']:
+                break
+            time.sleep(1)
+
+        if read_result.status == OperationStatusCodes.succeeded:
+            text = ""
+            for text_result in read_result.analyze_result.read_results:
+                for line in text_result.lines:
+                    text += line.text + "\n"
+            return text
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return ""
+
+def extract_text(DocketNo, file_pdf, k, data_folder, computervision_client):
+    temp_filename = 'temp.pdf'
+    cleanup_temp_file(temp_filename)
+    
+    """Coordinates the extraction of text from a PDF using Azure Computer Vision."""
+    source_file = os.path.join(data_folder, DocketNo, file_pdf)
+    
+    extract_pages(source_file, k)
+    text = read_pdf_with_azure(temp_filename, computervision_client)
+    cleanup_temp_file(temp_filename)  # Optional: Clean up again after reading
+
+    return text
+
 

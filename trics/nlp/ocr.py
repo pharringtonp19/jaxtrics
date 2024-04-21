4978aac91d33845b66a7c0461f8e5e808d8380fd
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-21 15:42:12 -0400

    Create ocr.py

---
+++ trics/nlp/ocr.py
@@ -0,0 +1,29 @@
+from typing import List
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+
+def get_text(doc: str, first_page: int = 1, last_page: int = 2) -> str:
+    """
+    Extracts text from specified pages of a PDF document using OCR.
+
+    Args:
+    doc (str): The file path to the PDF document.
+    first_page (int): The first page to start converting for text extraction.
+    last_page (int): The last page to convert for text extraction.
+
+    Returns:
+    str: The extracted text from the specified range of pages of the PDF.
+    """
+    # Initialize an empty string to hold all the text
+    text = ''
+
+    # Convert the pdf pages to images
+    images = convert_from_path(doc, first_page=first_page, last_page=last_page)
+
+    for image in images:
+        # Convert each image to text
+        text += pytesseract.image_to_string(image, lang='eng')
+
+    return text
+

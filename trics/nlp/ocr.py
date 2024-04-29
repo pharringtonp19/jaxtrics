a682ef2720829334165575b59f8afe4de83f49ac
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-29 15:55:49 -0400

    Update ocr.py

--- trics/nlp/ocr.py
+++ trics/nlp/ocr.py
@@ -4,7 +4,9 @@ from pdf2image import convert_from_path
 from PIL import Image
 import pytesseract
 from PyPDF2 import PdfReader, PdfWriter
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from typing import Optional
 import time
 
 def extract_first_k_pages(source_file: str, temp_file: str, num_pages: int) -> None:
@@ -29,8 +31,19 @@ def extract_first_k_pages(source_file: str, temp_file: str, num_pages: int) -> N
         pdf_writer.write(temp_pdf)
 
 
-def read_pdf_with_azure(temp_filename, computervision_client):
-    """Uses Azure Computer Vision to read text from a PDF stored in 'temp_filename'."""
+
+
+def read_pdf_with_azure(temp_filename: str, computervision_client: ComputerVisionClient) -> Optional[str]:
+    """
+    Uses Azure Computer Vision to read text from a PDF stored in 'temp_filename'.
+
+    Args:
+    temp_filename (str): The path to the temporary PDF file.
+    computervision_client (ComputerVisionClient): An instance of Azure's Computer Vision client.
+
+    Returns:
+    Optional[str]: The extracted text if the process succeeds, otherwise None.
+    """
     try:
         with open(temp_filename, "rb") as pdf:
             read_response = computervision_client.read_in_stream(pdf, raw=True)
@@ -51,7 +64,8 @@ def read_pdf_with_azure(temp_filename, computervision_client):
             return text
     except Exception as e:
         print(f"An error occurred: {e}")
-        return ""
+        return None
+
 
 def extract_text(data_folder, temp_file, DocketNo, file_pdf, intitial_k_pages, computervision_client):
 
